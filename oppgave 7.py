9c6a61bd27f745d055e3a42b02551c2bd54d536b
vebis-bit <58269612+vebis-bit@users.noreply.github.com>
2020-01-24 12:32:07 +0100

    innleveringer

--- oppgave 7.py
+++ oppgave 7.py
@@ -9,8 +9,8 @@ def newton(f,fder,t, v0,theta, tol=1E-8):
     teller = 0
     while abs(y(t,theta,v0)) > tol and teller < 100:
         t = t- y(t,theta,v0)/yder(t, theta, v0)
-        teller += 1
-    return t, teller
+        teller+=1
+    return t
 
 def y(t,theta,v0):
     g = 9.81
@@ -20,9 +20,20 @@ def yder(t, theta, v0):
     g = 9.81
     return v0*sin(theta) - g*t
 
-def x(t,v0):
+def x(t,v0, theta):
     return v0*cos(theta)*t
 
 tid = newton(y,yder,10, 18, pi/10)
-
-print(tid)
+avstand = x(tid, 18, pi/10)
+print("etter: ",tid,"s, kom ballen", avstand, "m")
+tid = newton(y,yder,10, 18, pi/6)
+avstand = x(tid, 18, pi/6)
+print("etter: ",tid,"s, kom ballen", avstand, "m")
+tid = newton(y,yder,10, 18, pi/5)
+avstand = x(tid, 18, pi/5)
+print("etter: ",tid,"s, kom ballen", avstand, "m")
+tid = newton(y,yder,10, 18, pi/4)
+avstand = x(tid, 18, pi/4)
+print("etter: ",tid,"s, kom ballen", avstand, "m")
+tid = newton(y,yder,10, 18, pi/3)
+avstand = x(tid, 18, pi/3)
--- primtall.py
+++ primtall.py
@@ -1,9 +1,7 @@
 from pylab import linspace
-from PIL import Image
-import numpy as np
 
-w,h = 250,250
-n = w*h
+
+n = 100000
 R = list(linspace(2, n, n-1))
 r = 0
 p = R[r]
@@ -16,16 +14,3 @@ while p**2 < n:
     r+=1
     print(r)
 print(R, r,len(R))
-
-
-data = np.zeros((h,w), dtype=np.uint8)
-
-for k in R:
-    x = int(k%w)
-    y = int(k/h)
-    data[y, x] = 255
-    
-
-img = Image.fromarray(data)
-img.show()
-
